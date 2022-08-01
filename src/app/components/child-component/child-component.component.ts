446d4b409a81b4d3ab8f66349b415fb7a6a60981
Mohamed Elhawy <engineermohamedelhawy@gmail.com>
2022-08-01 04:29:46 +0200

    input and output decor, ngOnChanges, viewChild

--- src/app/components/child-component/child-component.component.ts
+++ src/app/components/child-component/child-component.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output , EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-child-component',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.css']
 })
-export class ChildComponentComponent implements OnInit {
+export class ChildComponentComponent implements OnInit , OnChanges {
+
+  count : number = 0;
+
+  @Input() data : string = "apple";
+  @Output() increase_count = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes : SimpleChanges)
+  {
+
+    console.log(changes);
+
+    if ( changes['data'].currentValue == "sign in" )
+    {
+      console.log("welcome, back master.");
+    }
+    else if ( changes["data"].currentValue == "log out" )
+    {
+      console.log("signed out!");
+    }
+
+  }
+
+
+  send_parent_inc_number()
+  {
+    this.count++;
+
+    this.increase_count.emit(this.count);
+
   }
 
 }
--- src/app/components/page-eleven/page-eleven.component.ts
+++ src/app/components/page-eleven/page-eleven.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-page-eleven',
   templateUrl: './page-eleven.component.html',
   styleUrls: ['./page-eleven.component.css']
 })
-export class PageElevenComponent implements OnInit {
+export class PageElevenComponent implements OnInit , AfterViewInit {
+
+
+  newData : string = "banana";
+
+  child_data : number = 0;
+
+  @ViewChild("textRef") text_element! : ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit()
+  {
+    
+  }
+
+
+  update_child_value(data : number)
+  {
+    this.child_data = data;
+  }
+
+
+  sign_in()
+  {
+    this.newData = "sign in";
+  }
+
+
+  sign_out()
+  {
+    this.newData = "log out";
+  }
+
+
+  change_text()
+  {
+    this.text_element.nativeElement.textContent = "new text :D using Angular @ViewChild.";
   }
 
 }
